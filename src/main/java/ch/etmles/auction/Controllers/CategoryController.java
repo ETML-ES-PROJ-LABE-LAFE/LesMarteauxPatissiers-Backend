b80339457d3114c3bb12b047001d258598c8be15
Evgeny Ignatieff <122090512+Djinius-W@users.noreply.github.com>
2024-05-06 13:36:00 +0200

    ajout du crossOrigin

--- src/main/java/ch/etmles/auction/Controllers/CategoryController.java
+++ src/main/java/ch/etmles/auction/Controllers/CategoryController.java
@@ -1,6 +1,7 @@
 package ch.etmles.auction.Controllers;
 
 import ch.etmles.auction.Entities.Category;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "localhost:8081")
 @RequestMapping("/categories")
 public class CategoryController {
 
--- src/main/java/ch/etmles/auction/Controllers/ItemController.java
+++ src/main/java/ch/etmles/auction/Controllers/ItemController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "localhost:8081")
 @RequestMapping("/items")
 public class ItemController {
 
